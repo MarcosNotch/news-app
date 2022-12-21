6a05aaf9c301d15e4167198505c672ff8ca01861
Marcos <marcoscanette1@gmail.com>
2022-12-21 11:32:50 -0300

    123

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,5 +1,5 @@
 import useFetchNews from '../../utils/hooks/useFetchNews';
-import NewsList from '../components/newsList/NewsList';
+import NewsList from '../components/NewsList/NewsList';
 
 
 export default async function SearchPage({searchParams}){
